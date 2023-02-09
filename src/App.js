525e4d232192fe8656e3fae4fad3866065f8d395
NightSeaBlue <nun9017@naver.com>
2023-02-09 15:41:26 +0900

    리액트 연습 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // 정적변수
+    let title = "우리동네";
+    // 변경 데이터 변수
+    // let [변수명, setter] =useState(초기값);
+    let [subTitle, setSubTitle] = useState("우리카페");
+
+    return (
+        <div className="App">
+            <div>{title}</div>
+            <div>{subTitle}</div>
+            <hr />
+            <button onClick={() => (title = "누구세요")}> 제목변경 </button>
+            <button onClick={() => (subTitle = "남의 카페")}> 카페명변경 </button>
+            <button onClick={() => setSubTitle("남의 카페")}> 카페명변경 </button>
+        </div>
+    );
 }
 
 export default App;
